f00fb6dc04ad864a03882841a97b4ee25fbeff69
erikapss <priscila.erika@gmail.com>
2023-09-20 11:24:46 -0300

    nomeCompleto

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,7 +1,16 @@
 public class MinhaClasse {
     
     public static void main (String [] args) {
-System.out.print ("Olá!!");
+        String primeiroNome = "Gleyson";
+        String segundoNome = "Sampaio";
+
+        String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+        System.out.println(nomeCompleto);
 
     }
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
+}    
+
 }
