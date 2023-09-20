169bac3a553e0690af290bddbd008f2667e3bb9d
erikapss <priscila.erika@gmail.com>
2023-09-20 20:02:18 -0300

    Atualizações 20.09

--- java-anatomia-classes/src/edu/erika/santander/BoletimEstudantil.java
+++ java-anatomia-classes/src/edu/erika/santander/BoletimEstudantil.java
@@ -1,3 +1,5 @@
+package edu.erika.santander;
+
 public class BoletimEstudantil {
 	public static void main(String[] args) {
 		int mediaFinal = 6;
--- java-anatomia-classes/src/edu/erika/santander/MinhaClasse.java
+++ java-anatomia-classes/src/edu/erika/santander/MinhaClasse.java
@@ -1,3 +1,4 @@
+package edu.erika.santander;
 public class MinhaClasse {
     
     public static void main (String [] args) {
